4f80c4e02d784d3a08af38895028471ae0feae3b
Gaurav Verma <gauravverma3108@gmail.com>
2025-09-11 22:29:48 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+// src/index.js - Remove BrowserRouter from here
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './App';
+import AppWrapper from './components/AppWrapper'; // ✅ Keep using AppWrapper
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      {/* ✅ No BrowserRouter here since AppWrapper has it */}
+      <AppWrapper />
+    </Provider>
   </React.StrictMode>
 );
